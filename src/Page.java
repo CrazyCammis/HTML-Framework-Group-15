e91e27f0567625904177e274d72b4589c327cf23
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2022-01-24 05:29:09 +0100

    addded some basic functunaility

--- src/Page.java
+++ src/Page.java
@@ -26,4 +26,21 @@ public class Page {
     }
 
 
+    public void generateArticle(String header, String paragraph, String id, String groupclass) {
+        String article = "<article id=\"" + id + "\" class= \"" + groupclass + "\">\n" +
+                "<header>" + header +
+                "</header>" +
+                " <p>" + paragraph + "</p>" +
+                "</article>";
+    }
+
+    public void generateNavList(String[] links, String[] text, String id, String groupclass) {
+        int size = links.length;
+        String nav = "<nav id=\"" + id + "\" class= \"" + groupclass + "\">\n  <ul> \n";
+        for (int i = 0; i < size; i++) {
+
+            nav = nav.concat("<li><a href=" + links[i] + ">" + text[i] + "</a></li>\n");
+        }
+        nav = nav.concat("</ul>\n   </nav>");
+    }
 }
